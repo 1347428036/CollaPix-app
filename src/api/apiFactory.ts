2561a992a9be7a35c8bb51215e9154dfb1b97493
Steven <1347428036@qq.com>
2025-02-18 16:37:59 +0800

    feat: add new endpionts, add new controllers

--- src/api/apiFactory.ts
+++ src/api/apiFactory.ts
@@ -1,6 +1,24 @@
 import httpSender from '@/request'
-import { UserControllerApi } from './api'
+import { UserControllerApi } from './api.ts'
+import { PictureControllerApi } from './api.ts'
+import { SpaceControllerApi } from './api.ts'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
-export const userController = new UserControllerApi(undefined, BASE_URL, httpSender)
+export const userController: UserControllerApi = new UserControllerApi(
+  undefined,
+  BASE_URL,
+  httpSender,
+)
+
+export const pictureController: PictureControllerApi = new PictureControllerApi(
+  undefined,
+  BASE_URL,
+  httpSender,
+)
+
+export const spaceController: SpaceControllerApi = new SpaceControllerApi(
+  undefined,
+  BASE_URL,
+  httpSender,
+)
