037e47b36556daf0fbc8fbdfcb012b9db8355e94
Steven <1347428036@qq.com>
2025-03-02 16:27:23 +0800

    feat(api): update base API and configure Vite proxy
    - Update BaseAPI class in src/api/base.ts
      - Refactor imports and class definitions
      - Replace double quotes with single quotes
      - Remove unused ServerMap interface
    
    - Configure Vite proxy in vite.config.ts
      - Add proxy settings for /api and /api/ws
      - Enable WebSocket proxy for /api/ws

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,28 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.31.26:8123',
+        changeOrigin: true,
+        rewrite: (path) => path, //.replace(/^\/api/, ''),
+      },
+      '/api/ws': {
+        target: 'ws://192.168.31.26:8123',
+        changeOrigin: true,
+        // Enable WebSocket proxy
+        ws: true,
+      },
     },
   },
 })
