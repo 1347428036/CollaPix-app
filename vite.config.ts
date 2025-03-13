67d945a2ddb4a9509a993c1fb90e82be6e43daba
Steven <1347428036@qq.com>
2025-03-13 22:18:05 +0800

    feat(env): add environment configuration files and update related settings
    - Add .env, .env.dev, and .env.prod files for different environments
    - Update .gitignore to exclude dist directory
    - Modify package.json scripts to use environment modes
    - Adjust request.ts to use VITE_BASE_URI instead of VITE_BASE_URL
    - Update tsconfig.json to include vite/client types
    - Refactor vite.config.ts to load environment variables based on mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    open: false,
-    proxy: {
-      '/api': {
-        target: 'http://192.168.31.26:8123',
-        changeOrigin: true,
-        rewrite: (path) => path, //.replace(/^\/api/, ''),
-      },
-      '/api/ws': {
-        target: 'ws://192.168.31.26:8123',
-        changeOrigin: true,
-        // Enable WebSocket proxy
-        ws: true,
+export default defineConfig((mode) => {
+  const env = loadEnv(mode.mode, process.cwd())
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      open: false,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path,
+        },
+        '/api/ws': {
+          target: env.VITE_API_WS_BASE_URL,
+          changeOrigin: true,
+          // Enable WebSocket proxy
+          ws: true,
+        },
+      },
+    },
+  }
 })
